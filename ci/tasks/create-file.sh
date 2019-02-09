2fc48f2d9116e2a56f9830fa8529a33aff6bba68
Umamaheswararao Meka <uma@olivetech.com>
2019-02-09 23:18:25 +0530

    Added ci and task

---
+++ ci/tasks/create-file.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir some-files
+echo "console.log(`file1`)" > file1.js
+echo "console.log(`file2`)" > file2.js
+echo "console.log(`file3`)" > file3.js
+echo "console.log(`file4`)" > file4.js
+
+echo "All files have been created successfully"
---
+++ ci/tasks/get-file.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ls -la some-files/
+
+cd some-files
+node file1.js
+node file2.js
+node file3.js
+node.file4.js
