0d2c0567cd9840d39062affadd69e5e103409f5b
Umamaheswararao Meka <uma@olivetech.com>
2019-02-09 23:54:14 +0530

    modified yml file

--- ci/tasks/create-file.sh
+++ ci/tasks/create-file.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir some-files
+cd some-files
+touch file1.js file2.js file3.js file4.js
 echo "console.log(`file1`)" > some-files/file1.js
 echo "console.log(`file2`)" > some-files/file2.js
 echo "console.log(`file3`)" > some-files/file3.js
