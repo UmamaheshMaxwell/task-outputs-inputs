4f6b6f759357fc462fc94d084eb66cb4acd0adb0
Umamaheswararao Meka <uma@olivetech.com>
2019-02-09 23:51:10 +0530

    modified yml file

--- ci/tasks/create-file.sh
+++ ci/tasks/create-file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir some-files
-echo "console.log(`file1`)" > file1.js
-echo "console.log(`file2`)" > file2.js
-echo "console.log(`file3`)" > file3.js
-echo "console.log(`file4`)" > file4.js
+echo "console.log(`file1`)" > some-files/file1.js
+echo "console.log(`file2`)" > some-files/file2.js
+echo "console.log(`file3`)" > some-files/file3.js
+echo "console.log(`file4`)" > some-files/file4.js
 
 echo "All files have been created successfully"
