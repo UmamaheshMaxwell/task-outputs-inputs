4dcd1c44aa08415a1e1eedb60d8c0f72c2933e41
Umamaheswararao Meka <uma@olivetech.com>
2019-02-10 13:24:28 +0530

    modified yml and shell files

--- ci/tasks/create-file.sh
+++ ci/tasks/create-file.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 
-mkdir some-files
-echo "console.log(`file1`)" > some-files/file1.js
-echo "console.log(`file2`)" > some-files/file2.js
-echo "console.log(`file3`)" > some-files/file3.js
-echo "console.log(`file4`)" > some-files/file4.js
+
+if [ -d some-files ]    
+then 
+    echo "dir present"
+else
+    echo "dir not present"
+    mkdir some-files
+    echo "console.log(`file1`)" > some-files/file1.js
+    echo "console.log(`file2`)" > some-files/file2.js
+    echo "console.log(`file3`)" > some-files/file3.js
+    echo "console.log(`file4`)" > some-files/file4.js
+fi
 
 ls some-files/*
 
