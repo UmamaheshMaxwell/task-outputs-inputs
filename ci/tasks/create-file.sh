5cc4b63b1bcd23ce490870db9d1684f1da6da58f
Umamaheswararao Meka <uma@olivetech.com>
2019-02-10 13:33:49 +0530

    modified create file

--- ci/tasks/create-file.sh
+++ ci/tasks/create-file.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-if [ -d some-files ]    
-then 
-    echo "dir present"
-else
-    echo "dir not present"
-    mkdir some-files
-    echo "console.log('file1')" > some-files/file1.js
-    echo "console.log('file2')" > some-files/file2.js
-    echo "console.log('file3')" > some-files/file3.js
-    echo "console.log('file4')" > some-files/file4.js
-fi
+rm -ef some-files
+mkdir some-files
+echo "console.log('file1')" > some-files/file1.js
+echo "console.log('file2')" > some-files/file2.js
+echo "console.log('file3')" > some-files/file3.js
+echo "console.log('file4')" > some-files/file4.js
 
 ls some-files/*
 
