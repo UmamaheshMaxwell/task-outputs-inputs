b3e2259119b2a73eb2944891557bf8410f7cad31
Umamaheswararao Meka <uma@olivetech.com>
2019-02-10 13:38:45 +0530

    modified create file

--- ci/tasks/create-file.sh
+++ ci/tasks/create-file.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mkdir some-files
-echo "console.log('file1')" > some-files/file1.js
-echo "console.log('file2')" > some-files/file2.js
-echo "console.log('file3')" > some-files/file3.js
-echo "console.log('file4')" > some-files/file4.js
+echo "console.log('Hello from file1')" > some-files/file1.js
+echo "console.log('Hello from file2')" > some-files/file2.js
+echo "console.log('Hello from file3')" > some-files/file3.js
+echo "console.log('Hello from file4')" > some-files/file4.js
 
 ls some-files/*
 
